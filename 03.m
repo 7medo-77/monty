40df94a6b305996e80abc09148cee05468ec2b87
habibaaborehab <144491243+habibaaborehab@users.noreply.github.com>
2024-01-22 04:39:22 +0200

    03_test

---
+++ 03.m
@@ -0,0 +1,18 @@
+push 0 Push 0 onto the stack
+push 1 Push 1 onto the stack
+
+push 2
+  push 3
+
+
+                           
+push 4
+
+    push 5    
+      push    6        
+      
+push 6 push 63
+push j2
+push pall
+pall
+

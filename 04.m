60de91f0c1940b240a6511806a891f74906fed5a
7medo-77 <130787539+7medo-77@users.noreply.github.com>
2024-01-22 04:26:12 +0200

    monty_test_1

---
+++ 04.m
@@ -0,0 +1,23 @@
+pall
+push 1
+pall
+push 2
+pall
+push 3
+pall
+push 4
+pall
+
+pop
+pop pop
+pop
+
+pall hello! :) we don't really care about what's after pall
+
+
+	// if (!param.line_string)
+	// {
+	// 	fprintf(stderr, "Error: malloc failed\n");
+	// 	exit(EXIT_FAILURE);
+	// 	return;
+	// }
